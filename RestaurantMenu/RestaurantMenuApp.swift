a7b339a01100c9a44508bf00350441939094b149
test <test>
2023-10-18 16:47:14 -0400

    Initial Commit

---
+++ RestaurantMenu/RestaurantMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuApp.swift
+//  RestaurantMenu
+//
+//  Created by macuser on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
